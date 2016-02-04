f37cd0127f9777eafb985bc2a61d69a6d4f9b8ae
Matt Leonawicz <matt.rstat@gmail.com>
2016-02-04 13:39:40 -0700

    Basic leaflet example for rasterized data.

---
+++ ex_leaflet.R
@@ -0,0 +1,9 @@
+library(raster)
+library(leaflet)
+load("workspaces/nwt_testing_subset.Rdata")
+
+r <- subset(x, 1)
+pal <- colorNumeric(c("#0C2C84", "#41B6C4", "#FFFFCC"), values(r), na.color="transparent")
+
+leaflet() %>% addTiles() %>% addRasterImage(r, colors=pal, opacity=0.8) %>%
+  addLegend(pal=pal, values=values(r), title="Precipitation (mm)")
