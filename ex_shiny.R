0239d0d92a67167027e00ffeaed485a62190db48
Matt Leonawicz <matt.rstat@gmail.com>
2016-02-04 13:40:02 -0700

    Basic leaflet shiny app example for rasterized data.

---
+++ ex_shiny.R
@@ -0,0 +1,49 @@
+library(shiny)
+library(raster)
+library(leaflet)
+load("workspaces/nwt_testing_subset.Rdata")
+
+r <- subset(x, 1)
+lon <- (xmin(r)+xmax(r))/2
+lat <- (ymin(r)+ymax(r))/2
+decades <- seq(2010, 2090, by=10)
+decade.names <- paste0(decades, "s")
+pal <- colorNumeric(c("#0C2C84", "#41B6C4", "#FFFFCC"), values(r), na.color="transparent")
+
+ui <- bootstrapPage(
+  tags$style(type="text/css", "html, body {width:100%;height:100%}"),
+  leafletOutput("Map", width="100%", height="100%"),
+  absolutePanel(top=10, right=10,
+    sliderInput("dec", "Decade", min=min(decades), max=max(decades), value=decades[1], step=10, sep="", post="s"),
+    checkboxInput("legend", "Show legend", TRUE)
+  )
+)
+
+server <- function(input, output, session) {
+
+  ras <- reactive({
+    subset(x, which(decades==input$dec))
+  })
+
+  ras_vals <- reactive({ values(ras()) })
+
+  output$Map <- renderLeaflet({
+    leaflet() %>% setView(lon, lat, 4) %>% addTiles()
+  })
+
+  observe({
+    proxy <- leafletProxy("Map")
+    proxy %>% removeTiles(layerId="rasimg") %>% addRasterImage(ras(), colors=pal, opacity=0.8, layerId="rasimg")
+  })
+
+  observe({
+    proxy <- leafletProxy("Map")
+    proxy %>% clearControls()
+    if (input$legend) {
+      proxy %>% addLegend(position="bottomright", pal=pal, values=ras_vals(), title="Precipitation", labFormat=labelFormat(suffix="mm"))
+    }
+  })
+
+}
+
+shinyApp(ui, server)
